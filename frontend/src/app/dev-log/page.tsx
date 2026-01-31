0e349a6cb149961ea5fdad8d3d646a94eacf0c5b
Guacamoleboy <cph-jl325@stud.ek.dk>
2026-01-31 01:46:01 +0100

    Small updates

---
+++ frontend/src/app/dev-log/page.tsx
@@ -0,0 +1,5 @@
+import DevLog from "@/components/dev-log/DevLogUI";
+
+export default async function ClinicPage() {
+    return <DevLog />;
+}
---
+++ frontend/src/components/dev-log/DevLogUI.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function DevLogUI() {
+  return (
+    <div style={{ padding: 50, textAlign: "center", fontSize: 32 }}>
+      Dev Log
+    </div>
+  );
+}
